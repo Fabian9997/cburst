2f2fc84ab571f9df5e64a7260895b93536768ff1
DonkVenom <83369645+DonkVenom@users.noreply.github.com>
2023-02-23 17:16:39 +0100

    Delete cburst.sh

--- cburst.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-size=$(du -sh ~/Library/Caches/ | awk '{print $1}') # awk cuts the second term out of the statement so that only the size of the folder is saved into the variable
-echo "The size of the folder is ${size} bytes."
-cd /Library/Caches/
-rm -rf *
-cd ~/Library/Caches/
-rm -rf *
-echo "Cache cleaned!"
